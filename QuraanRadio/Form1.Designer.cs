3952b0916a0251a99b622b0017b56080eb4c406c
Ayoob Ali <Github@ayoobali.com>
2015-01-20 12:37:38 +0400

    Project Added

---
+++ QuraanRadio/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace QuraanRadio
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.LblExit = new System.Windows.Forms.Label();
+            this.LblMin = new System.Windows.Forms.Label();
+            this.LblPlay = new System.Windows.Forms.Label();
+            this.LblPause = new System.Windows.Forms.Label();
+            this.LblNext = new System.Windows.Forms.Label();
+            this.LblDownload = new System.Windows.Forms.Label();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.prgBar = new System.Windows.Forms.ProgressBar();
+            this.LblAbout = new System.Windows.Forms.Label();
+            this.cMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.languagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.allToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LblExit
+            // 
+            this.LblExit.BackColor = System.Drawing.Color.Transparent;
+            this.LblExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LblExit.Location = new System.Drawing.Point(285, 26);
+            this.LblExit.Name = "LblExit";
+            this.LblExit.Size = new System.Drawing.Size(19, 11);
+            this.LblExit.TabIndex = 0;
+            this.LblExit.Click += new System.EventHandler(this.LblExit_Click);
+            // 
+            // LblMin
+            // 
+            this.LblMin.BackColor = System.Drawing.Color.Transparent;
+            this.LblMin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LblMin.Location = new System.Drawing.Point(260, 26);
+            this.LblMin.Name = "LblMin";
+            this.LblMin.Size = new System.Drawing.Size(19, 11);
+            this.LblMin.TabIndex = 1;
+            this.LblMin.Click += new System.EventHandler(this.LblMin_Click);
+            // 
+            // LblPlay
+            // 
+            this.LblPlay.BackColor = System.Drawing.Color.Transparent;
+            this.LblPlay.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LblPlay.Location = new System.Drawing.Point(86, 51);
+            this.LblPlay.Name = "LblPlay";
+            this.LblPlay.Size = new System.Drawing.Size(35, 31);
+            this.LblPlay.TabIndex = 2;
+            this.LblPlay.Click += new System.EventHandler(this.LblPlay_Click);
+            // 
+            // LblPause
+            // 
+            this.LblPause.BackColor = System.Drawing.Color.Transparent;
+            this.LblPause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LblPause.Location = new System.Drawing.Point(127, 51);
+            this.LblPause.Name = "LblPause";
+            this.LblPause.Size = new System.Drawing.Size(45, 31);
+            this.LblPause.TabIndex = 3;
+            this.LblPause.Click += new System.EventHandler(this.LblPause_Click);
+            // 
+            // LblNext
+            // 
+            this.LblNext.BackColor = System.Drawing.Color.Transparent;
+            this.LblNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LblNext.Location = new System.Drawing.Point(178, 51);
+            this.LblNext.Name = "LblNext";
+            this.LblNext.Size = new System.Drawing.Size(50, 31);
+            this.LblNext.TabIndex = 4;
+            this.LblNext.Click += new System.EventHandler(this.LblNext_Click);
+            // 
+            // LblDownload
+            // 
+            this.LblDownload.BackColor = System.Drawing.Color.Transparent;
+            this.LblDownload.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LblDownload.Location = new System.Drawing.Point(234, 51);
+            this.LblDownload.Name = "LblDownload";
+            this.LblDownload.Size = new System.Drawing.Size(35, 31);
+            this.LblDownload.TabIndex = 5;
+            this.LblDownload.Click += new System.EventHandler(this.LblDownload_Click);
+            // 
+            // prgBar
+            // 
+            this.prgBar.BackColor = System.Drawing.Color.Gray;
+            this.prgBar.Location = new System.Drawing.Point(47, 26);
+            this.prgBar.Name = "prgBar";
+            this.prgBar.Size = new System.Drawing.Size(49, 10);
+            this.prgBar.TabIndex = 6;
+            this.prgBar.Visible = false;
+            // 
+            // LblAbout
+            // 
+            this.LblAbout.BackColor = System.Drawing.Color.Transparent;
+            this.LblAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LblAbout.Location = new System.Drawing.Point(285, 42);
+            this.LblAbout.Name = "LblAbout";
+            this.LblAbout.Size = new System.Drawing.Size(19, 11);
+            this.LblAbout.TabIndex = 7;
+            this.LblAbout.Click += new System.EventHandler(this.LblAbout_Click);
+            // 
+            // cMenu
+            // 
+            this.cMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languagesToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.cMenu.Name = "cMenu";
+            this.cMenu.Size = new System.Drawing.Size(153, 70);
+            // 
+            // languagesToolStripMenuItem
+            // 
+            this.languagesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.allToolStripMenuItem,
+            this.englishToolStripMenuItem});
+            this.languagesToolStripMenuItem.Name = "languagesToolStripMenuItem";
+            this.languagesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.languagesToolStripMenuItem.Text = "Languages";
+            // 
+            // allToolStripMenuItem
+            // 
+            this.allToolStripMenuItem.Name = "allToolStripMenuItem";
+            this.allToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.allToolStripMenuItem.Text = "All";
+            this.allToolStripMenuItem.Click += new System.EventHandler(this.allToolStripMenuItem_Click);
+            // 
+            // englishToolStripMenuItem
+            // 
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.englishToolStripMenuItem.Text = "English";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = global::QuraanRadio.Properties.Resources.app1;
+            this.ClientSize = new System.Drawing.Size(337, 91);
+            this.Controls.Add(this.LblAbout);
+            this.Controls.Add(this.prgBar);
+            this.Controls.Add(this.LblDownload);
+            this.Controls.Add(this.LblNext);
+            this.Controls.Add(this.LblPause);
+            this.Controls.Add(this.LblPlay);
+            this.Controls.Add(this.LblMin);
+            this.Controls.Add(this.LblExit);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quraan Radio";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.cMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Label LblExit;
+        private System.Windows.Forms.Label LblMin;
+        private System.Windows.Forms.Label LblPlay;
+        private System.Windows.Forms.Label LblPause;
+        private System.Windows.Forms.Label LblNext;
+        private System.Windows.Forms.Label LblDownload;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.ProgressBar prgBar;
+        private System.Windows.Forms.Label LblAbout;
+        private System.Windows.Forms.ContextMenuStrip cMenu;
+        private System.Windows.Forms.ToolStripMenuItem languagesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem allToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ QuraanRadio/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.IO;
+
+namespace QuraanRadio
+{
+
+    public partial class FrmMain : Form
+    {
+        // Move Form on mouse Down
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,
+                         int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public AXVLC.VLCPlugin2 vlcP = new AXVLC.VLCPlugin2();
+
+        public static string URL = "http://ayoobali.com/quraan/radio.php?url=yes";
+        public static string fileURL;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            vlcP.MediaPlayerEndReached += vlcP_MediaPlayerEndReached;
+        }
+
+        void vlcP_MediaPlayerEndReached()
+        {
+            GetFile();
+        }
+
+
+
+        public void GetFile()
+        {
+            
+
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(URL);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            fileURL = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+
+            if (fileURL.Substring(0, 4) == "http")
+            {
+                if (vlcP.playlist.items.count >= 1)
+                {
+                    if (vlcP.playlist.isPlaying) 
+                    {
+                        vlcP.playlist.stop();
+                    }
+
+                    vlcP.playlist.items.clear();
+                }
+
+                vlcP.playlist.add(fileURL);
+                vlcP.playlist.play();
+                vlcP.Volume = 100;
+                
+
+            }
+            //if (fileURL.Substring(0, 4) != "http")
+            //{
+            //    GetFile();
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string URLv = "http://ayoobali.com/quraan/radio.php?version=101";
+
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(URLv);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string versionCheck = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+
+            if (versionCheck != "Updated" && versionCheck.Substring(0, 1) == "v")
+            {
+                MessageBox.Show("Newer version is available: " + versionCheck + "\n\nVisit http://ayoobali.com to get the new update.");
+                
+            }
+
+            GetFile();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void LblExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void LblMin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FrmMain.ActiveForm.WindowState = FormWindowState.Minimized;
+            }
+            catch{}
+            
+        }
+
+        private void LblPause_Click(object sender, EventArgs e)
+        {
+
+            vlcP.playlist.pause();
+        }
+
+        private void LblNext_Click(object sender, EventArgs e)
+        {
+            GetFile();
+        }
+
+        private void LblPlay_Click(object sender, EventArgs e)
+        {
+            vlcP.playlist.play();
+        }
+
+        private void LblDownload_Click(object sender, EventArgs e)
+        {
+            
+            string[] spName = fileURL.Split('/');
+
+            saveFile.Title = "Save Sura As";
+            saveFile.Filter = "MP3|*.mp3";
+            saveFile.FileName = spName[spName.Length - 1];
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                prgBar.Value = 0;
+                prgBar.Visible = true;
+                WebClient downloadFile = new WebClient();
+                downloadFile.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                downloadFile.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                downloadFile.DownloadFileAsync(new Uri(fileURL), saveFile.FileName);
+
+            }
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            prgBar.Value = e.ProgressPercentage;
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            prgBar.Visible = false;
+        }
+
+        private void LblAbout_Click(object sender, EventArgs e)
+        {
+            
+            cMenu.Show(Cursor.Position.X,Cursor.Position.Y);
+
+            
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string URLv = "http://ayoobali.com/quraan/radio.php?version=101";
+
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(URLv);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string versionCheck = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+
+            string AboutMSG = "Quraan Radio v1.0.1\nDeveloped By: AyoobAli.com\n\nMedia from Islamway.com";
+
+            if (versionCheck != "Updated" && versionCheck.Substring(0, 1) == "v")
+            {
+                AboutMSG = AboutMSG + "\n\nNew version is available: " + versionCheck;
+            }
+            MessageBox.Show(AboutMSG);
+        }
+
+        private void allToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            URL = "http://ayoobali.com/quraan/radio.php?url=yes";
+            GetFile();
+        }
+
+        private void englishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            URL = "http://ayoobali.com/quraan/radio.php?url=yes&lang=en";
+            GetFile();
+        }
+
+
+    }
+}
